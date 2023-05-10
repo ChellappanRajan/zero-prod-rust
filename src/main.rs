27b747fb3895f72a1852a1ed39d5048f89b306b0
Chellappan <vrchellappan@gmail.com>
2023-05-10 13:11:29 +0530

    add health_check endpoint route

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+//impl Responder trait meaning we are returing any type that returing Responder trait
+//Question:: How come rust knows format implementing Responder trait?
+//“Responder is nothing more than a conversion trait into a HttpResponse.”
+//any type that implement Responder should return this function
+//Since HttpResponse impl this trait we can return HttpRespone
+async fn health_checker(req: HttpRequest) -> impl Responder{
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    //HttpServer handle all transport level concerns
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            // .route("/", web::get().to(greet))
+            // .route("/{name}", web::get().to(greet))
+        .route("/health_check", web::get().to(health_checker))
     })
     .bind("127.0.0.1:8000")?
     .run()
