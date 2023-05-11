108235ad4bdd40f38dc12e332a4830cfeaf15ead
Chellappan <vrchellappan@gmail.com>
2023-05-11 22:19:36 +0530

    Add dev dependencies for testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,1 +1,20 @@
-// use super::main;
+
+#[tokio::test]
+async fn health_check(){
+    // Arrange
+    spawn_app().await.expect("Failed to spawn our app.");
+
+    let client = reqwest::Client::new();
+
+    //Act
+    let response = client.get("http://127.0.0.1:8000/health_check").send().await.expect("Failed to excute test");
+
+    //Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app()->std::io::Result<()>{
+ todo!()
+}
+    
