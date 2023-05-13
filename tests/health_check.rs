777c551385e78cff6196ffb8e212d7ce87b05c80
Chellappan <vrchellappan@gmail.com>
2023-05-13 13:57:40 +0530

    Add parameterized test or table test drive test cases for form

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,4 @@
-use std::{net::TcpListener, fmt::format};
+use std::{net::TcpListener, fmt::format, assert_eq};
 
 
 #[tokio::test]
@@ -17,6 +17,60 @@ async fn health_check(){
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data(){
+    //Arrang
+    let app_address = format!("{}/subscriptions",spawn_app());
+    let client = reqwest::Client::new();
+
+    //Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client.post(app_address)
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .body(body)
+    .send()
+    .await
+    .expect("Failded to excute request.");
+ 
+   //Assert
+   assert_eq!(200,response.status().as_u16());
+
+}
+
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing(){
+    //Arrang
+    let app_address = format!("{}/subscriptions",spawn_app());
+    let client = reqwest::Client::new(); 
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for(invalid_body,error_msg) in test_cases{
+    //Act
+    let response = client.post(&app_address)
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .body(invalid_body)
+    .send()
+    .await
+    .expect("Failded to excute request.");
+
+    //Assert
+    assert_eq!(200,response.status().as_u16());
+    }
+
+
+    
+ 
+   
+
+}
+
+
+
  fn spawn_app()->String{
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failde to bind random port");
     let port = listener.local_addr().unwrap().port();
@@ -25,3 +79,4 @@ async fn health_check(){
  format!("http://127.0.0.1:{}",port)
 }
     
+
