715a0a84bccc13ca2e58c6fc461ad9d3b0b6b23d
Chellappan <vrchellappan@gmail.com>
2023-05-21 10:10:42 +0530

    Add log to subscribe

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,6 +16,9 @@ pub async fn subscribe(
   //Retrieving connection from app state
   pool:web::Data<PgPool>
 )->HttpResponse{
+  let request_id =  Uuid::new_v4();
+  log::info!("request_id {}- Adding '{}' '{}' as a new subscriber.",request_id,form.email,form.name);
+  log::info!("Saving new subscriber details in the database");
   match sqlx::query!(
     r#"
     INSERT INTO subscriptions (id,email,name,subscribed_at)
@@ -26,9 +29,12 @@ pub async fn subscribe(
     form.name,
     Utc::now()
   ).execute(pool.get_ref()).await{
-    Ok(_)=> HttpResponse::Ok().finish(),
+    Ok(_)=> {
+      log::info!("request_id {} - New subscribe have been saved",request_id);
+      HttpResponse::Ok().finish()
+    },
     Err(e)=>{
-      println!("Failed to excute query,{}",e);
+      log::info!("request_id {}, Failed to excute query,{:?}",request_id,e);
       HttpResponse::InternalServerError().finish()
     }
   }
