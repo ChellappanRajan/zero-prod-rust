1d231a8edec6da4fc0e80c300b4a1285004c61f0
Chellappan <vrchellappan@gmail.com>
2023-05-21 08:47:11 +0530

    Fix env_logger dep issue

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 
 use std::net::TcpListener;
-
-use sqlx::{PgConnection, Connection, PgPool};
+use sqlx::{PgPool};
 use zeroProdRust::startup::run;
-
+use env_logger::Env;
 use zeroProdRust::configuration::{get_configurations};
 
 
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let configuration = get_configurations().expect("Failed to Read config");
 
